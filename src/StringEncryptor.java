1ca5794eca610475e9fb36d62e370322f4e88130
Kaden <kho7@huskers.unl.edu>
2025-07-12 19:50:37 -0500

    Allow users to enter plaintext to be encrypted and select an encryption method

---
+++ src/StringEncryptor.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class StringEncryptor {
+    public static void main(String[] args) {
+        String plaintext = retrieveString();
+        String chosenEncryptionMethod = retrieveEncryptionMethod();
+        switch (chosenEncryptionMethod) {
+            case "Caesar Cipher":
+                System.out.println("Caesar Cipher");
+                break;
+            case "Vigenere Cipher":
+                System.out.println("Vigenere Cipher");
+                break;
+            case "Mixed Alphabet Cipher":
+                System.out.println("Mixed Alphabet Cipher");
+                break;
+            default:
+                System.out.println("Error: Unable to find encryption method");
+                System.exit(1);
+        }
+    }
+
+    private static String retrieveString() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter plaintext string: ");
+        return scanner.nextLine();
+    }
+
+    private static String retrieveEncryptionMethod() {
+        Scanner scanner = new Scanner(System.in);
+        String[] availableEncryptionMethods = {"Caesar Cipher", "Vigenere Cipher",
+                "Mixed Alphabet Cipher"};
+        for (int i = 0; i < availableEncryptionMethods.length; i++) {
+            System.out.println("[" + (i + 1) + "] " + availableEncryptionMethods[i]);
+        }
+        System.out.print("Enter encryption method: ");
+        int retrievedAnswer;
+        try {
+            retrievedAnswer = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid entry, must enter an integer");
+            return retrieveEncryptionMethod();
+        } try {
+            return availableEncryptionMethods[retrievedAnswer - 1];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid entry, must be between 1 and " + availableEncryptionMethods.length);
+            return retrieveEncryptionMethod();
+        }
+    }
+}
