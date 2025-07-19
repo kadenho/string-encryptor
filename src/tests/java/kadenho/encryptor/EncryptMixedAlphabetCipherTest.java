a1b8441c88d8543b409ea86b4c1b374376e5e46c
Kaden <kho7@huskers.unl.edu>
2025-07-19 10:46:38 -0500

    rename mixedAlphabetCipher to encryptMixedAlphabetCipher, and mixedAlphabetCipherTest to EncryptMixedAlphabetCipherTest

--- src/tests/java/kadenho/encryptor/EncryptMixedAlphabetCipherTest.java
+++ src/tests/java/kadenho/encryptor/EncryptMixedAlphabetCipherTest.java
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-public class MixedAlphabetCipherTest {
+public class EncryptMixedAlphabetCipherTest {
     @Test
     public void lowercaseTextLowercaseKey() {
         String plaintext = "helloworld";
         String[] expectedOutput = {"ryffjujnfb", "goodbyeworld"};
-        String[] actualOutput = StringEncryptor.mixedAlphabetCipher(plaintext, "goodbyeworld");
+        String[] actualOutput = StringEncryptor.encryptMixedAlphabetCipher(plaintext, "goodbyeworld");
         assertArrayEquals(expectedOutput, actualOutput);
     }
 
@@ -17,7 +17,7 @@ public class MixedAlphabetCipherTest {
     public void lowercaseTextMixcaseKey() {
         String plaintext = "softwareengineering";
         String[] expectedOutput = {"kftlwcjuudesduujsde", "ComputerScience"};
-        String[] actualOutput = StringEncryptor.mixedAlphabetCipher(plaintext, "ComputerScience");
+        String[] actualOutput = StringEncryptor.encryptMixedAlphabetCipher(plaintext, "ComputerScience");
         assertArrayEquals(expectedOutput, actualOutput);
     }
 
@@ -25,7 +25,7 @@ public class MixedAlphabetCipherTest {
     public void mixcaseTextLowercaseKey() {
         String plaintext = "PaNcAkEs";
         String[] expectedOutput = {"NwKfWhEq", "waffles"};
-        String[] actualOutput = StringEncryptor.mixedAlphabetCipher(plaintext, "waffles");
+        String[] actualOutput = StringEncryptor.encryptMixedAlphabetCipher(plaintext, "waffles");
         assertArrayEquals(expectedOutput, actualOutput);
     }
 
@@ -33,7 +33,7 @@ public class MixedAlphabetCipherTest {
     public void mixcaseTextMixcaseKey() {
         String plaintext = "CamelCaseRules";
         String[] expectedOutput = {"KihsgKirsQugsr", "iLikeSnakeCase"};
-        String[] actualOutput = StringEncryptor.mixedAlphabetCipher(plaintext, "iLikeSnakeCase");
+        String[] actualOutput = StringEncryptor.encryptMixedAlphabetCipher(plaintext, "iLikeSnakeCase");
         assertArrayEquals(expectedOutput, actualOutput);
     }
 
@@ -41,7 +41,7 @@ public class MixedAlphabetCipherTest {
     public void lowercaseSpacedTextLowercaseKey() {
         String plaintext = "i have many favorite artists";
         String[] expectedOutput = {"e dcvr jcky ocvmqetr cqtests", "clairo"};
-        String[] actualOutput = StringEncryptor.mixedAlphabetCipher(plaintext, "clairo");
+        String[] actualOutput = StringEncryptor.encryptMixedAlphabetCipher(plaintext, "clairo");
         assertArrayEquals(expectedOutput, actualOutput);
     }
 
@@ -49,7 +49,7 @@ public class MixedAlphabetCipherTest {
     public void lowercaseSpacedTextMixcaseKey() {
         String plaintext = "its hard to pick just one to be my favorite";
         String[] expectedOutput = {"hts gbrd tn phaj iust nmo tn eo ly cbvnrhto", "Beabadoobee"};
-        String[] actualOutput = StringEncryptor.mixedAlphabetCipher(plaintext, "Beabadoobee");
+        String[] actualOutput = StringEncryptor.encryptMixedAlphabetCipher(plaintext, "Beabadoobee");
         assertArrayEquals(expectedOutput, actualOutput);
     }
 
@@ -57,7 +57,7 @@ public class MixedAlphabetCipherTest {
     public void mixcaseSpacedTextLowercaseKey() {
         String plaintext = "For example I love the band Bleachers but I also like the band";
         String[] expectedOutput = {"Rko axtglfa B fkva qsa htjm Hfatesaop huq B tfpk fbda qsa htjm", "themarias"};
-        String[] actualOutput = StringEncryptor.mixedAlphabetCipher(plaintext, "themarias");
+        String[] actualOutput = StringEncryptor.encryptMixedAlphabetCipher(plaintext, "themarias");
         assertArrayEquals(expectedOutput, actualOutput);
     }
 
@@ -65,7 +65,7 @@ public class MixedAlphabetCipherTest {
     public void mixcaseSpacedTextMixcaseKey() {
         String plaintext = "I think Sabrina Carpenter and Suki Waterhouse are great too";
         String[] expectedOutput = {"C tbckg Sinrcki Dirmpktpr ike Sugc Witprblusp irp arpit tll", "IndiePop"};
-        String[] actualOutput = StringEncryptor.mixedAlphabetCipher(plaintext, "IndiePop");
+        String[] actualOutput = StringEncryptor.encryptMixedAlphabetCipher(plaintext, "IndiePop");
         assertArrayEquals(expectedOutput, actualOutput);
     }
 
@@ -73,7 +73,7 @@ public class MixedAlphabetCipherTest {
     public void lowercaseSpacedMixCharTextLowercaseKey() {
         String plaintext = "my favorite movie franchise is paddington 2. it is very cute and silly!";
         String[] expectedOutput = {"fy osvhmnqi fhvni omsguanpi np jsddngcqhg 2. nq np vimy urqi sgd pneey!", "studiocanal"};
-        String[] actualOutput = StringEncryptor.mixedAlphabetCipher(plaintext, "studiocanal");
+        String[] actualOutput = StringEncryptor.encryptMixedAlphabetCipher(plaintext, "studiocanal");
         assertArrayEquals(expectedOutput, actualOutput);
     }
 
@@ -81,7 +81,7 @@ public class MixedAlphabetCipherTest {
     public void lowercaseSpacedMixCharTextMixcaseKey() {
         String plaintext = "which paddington movie do you like the most: 1, 2, or 3? they are all very great movies.";
         String[] expectedOutput = {"wcfrc omllfkbtnk jnvfd ln ynu ifhd tcd jnst: 1, 2, nq 3? tcdy mqd mii vdqy bqdmt jnvfds.", "Marmalade"};
-        String[] actualOutput = StringEncryptor.mixedAlphabetCipher(plaintext, "Marmalade");
+        String[] actualOutput = StringEncryptor.encryptMixedAlphabetCipher(plaintext, "Marmalade");
         assertArrayEquals(expectedOutput, actualOutput);
     }
 
@@ -89,7 +89,7 @@ public class MixedAlphabetCipherTest {
     public void mixcaseSpacedMixCharTextLowercaseKey() {
         String plaintext = "I like many genres of movies, but especially horror. They keep me on the edge of my seat!";
         String[] expectedOutput = {"F ifha jsky bakpaq lm jlvfaq, cut aqnarfsiiy dlpplp. Tday haan ja lk tda aeba lm jy qast!", "scream"};
-        String[] actualOutput = StringEncryptor.mixedAlphabetCipher(plaintext, "scream");
+        String[] actualOutput = StringEncryptor.encryptMixedAlphabetCipher(plaintext, "scream");
         assertArrayEquals(expectedOutput, actualOutput);
     }
 
@@ -97,7 +97,7 @@ public class MixedAlphabetCipherTest {
     public void mixcaseSpacedMixCharTextMixcaseKey() {
         String plaintext = "The most recent movie I watched was 20th Century Girl. It had a very sad ending.";
         String[] expectedOutput = {"Ter jlst qrbrkt jlvfr F wntbero wns 20te Brktuqy Dfqi. Ft eno n vrqy sno rkofkd.", "NaBoRa"};
-        String[] actualOutput = StringEncryptor.mixedAlphabetCipher(plaintext, "NaBoRa");
+        String[] actualOutput = StringEncryptor.encryptMixedAlphabetCipher(plaintext, "NaBoRa");
         assertArrayEquals(expectedOutput, actualOutput);
     }
 
