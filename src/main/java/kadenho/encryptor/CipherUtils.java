4fe26392d8b359098e6a616b5ea412d0ddcae86b
Kaden Ho <kho7@huskers.unl.edu>
2025-09-14 17:04:17 -0500

    Move convertAlphabet to CipherUtils

--- src/main/java/kadenho/encryptor/CipherUtils.java
+++ src/main/java/kadenho/encryptor/CipherUtils.java
@@ -15,4 +15,21 @@ public class CipherUtils {
         }
         return multiplicativeInverse;
     }
+
+    public static String convertAlphabets(String plaintext, String currentAlphabet, String newAlphabet) {
+        StringBuilder ciphertextStringBuilder = new StringBuilder();
+        for (char character : plaintext.toCharArray()) {
+            int characterIndex = currentAlphabet.indexOf(Character.toLowerCase(character));
+            if (characterIndex >= 0) {
+                char scrambledCharacter = newAlphabet.charAt(characterIndex);
+                if (Character.isUpperCase(character)) {
+                    scrambledCharacter = Character.toUpperCase(scrambledCharacter);
+                }
+                ciphertextStringBuilder.append(scrambledCharacter);
+            } else {
+                ciphertextStringBuilder.append(character);
+            }
+        }
+        return ciphertextStringBuilder.toString();
+    }
 }
